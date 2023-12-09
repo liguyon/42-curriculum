ccc0e995213e978eae9c17fee112b3d20e6b3b21
liguyon <liguyon@student.42lehavre.fr>
2023-12-09 12:43:15 +0100

    fix an error where the forks were given back two times

--- 3-philosophers/philo/src/philo_actions.c
+++ 3-philosophers/philo/src/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: liguyon <liguyon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 06:43:33 by liguyon           #+#    #+#             */
-/*   Updated: 2023/12/09 10:49:13 by liguyon          ###   ########.fr       */
+/*   Updated: 2023/12/09 12:38:47 by liguyon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	philo_think(t_data *data, t_philo *philo, long long time_now)
 {
+	philo->state = STATE_THINK;
+	printf("%lld %d is thinking\n",
+		time_now - data->time_start, philo->id);
+}
+
+void	philo_sleep(t_data *data, t_philo *philo, long long time_now)
+{
+	philo->state = STATE_SLEEP;
 	pthread_mutex_lock(&philo->right->mutex);
 	philo->right->taken = false;
 	pthread_mutex_unlock(&philo->right->mutex);
@@ -22,20 +30,6 @@ void	philo_think(t_data *data, t_philo *philo, long long time_now)
 	philo->left->taken = false;
 	pthread_mutex_unlock(&philo->left->mutex);
 	philo->has_left = false;
-	philo->state = STATE_THINK;
-	printf("%lld %d is thinking\n",
-		time_now - data->time_start, philo->id);
-}
-
-void	philo_sleep(t_data *data, t_philo *philo, long long time_now)
-{
-	pthread_mutex_lock(&philo->right->mutex);
-	philo->right->taken = false;
-	pthread_mutex_unlock(&philo->right->mutex);
-	pthread_mutex_lock(&philo->left->mutex);
-	philo->left->taken = false;
-	pthread_mutex_unlock(&philo->left->mutex);
-	philo->state = STATE_SLEEP;
 	printf("%lld %d is sleeping\n",
 		time_now - data->time_start, philo->id);
 	philo->time_start_sleep = time_now;
