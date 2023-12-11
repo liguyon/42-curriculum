cd14925bd9f35b586006b49f550ddcec93209a70
liguyon <liguyon@student.42lehavre.fr>
2023-12-11 04:21:39 +0100

    minishell: testing readline

--- 3-minishell/src/main.c
+++ 3-minishell/src/main.c
@@ -6,18 +6,25 @@
 /*   By: liguyon <liguyon@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 03:36:08 by liguyon           #+#    #+#             */
-/*   Updated: 2023/12/11 03:38:13 by liguyon          ###   ########.fr       */
+/*   Updated: 2023/12/11 04:04:36 by liguyon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	printf("hello\n");
+	while (1)
+	{
+		char *s = readline("$ ");
+		printf("%s\n", s);
+		free(s);
+	}
 	return (0);
 }
