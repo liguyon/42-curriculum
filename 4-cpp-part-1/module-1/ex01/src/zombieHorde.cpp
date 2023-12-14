0203181756ae3ad90eaebcd7639f9edd9c9743ea
liguyon <liguyon@student.42lehavre.fr>
2023-12-14 16:35:50 +0100

    cpp-1: ex02 done

--- 4-cpp-part-1/module-1/ex01/src/zombieHorde.cpp
+++ 4-cpp-part-1/module-1/ex01/src/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: liguyon <liguyon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:40:58 by liguyon           #+#    #+#             */
-/*   Updated: 2023/12/14 16:07:45 by liguyon          ###   ########.fr       */
+/*   Updated: 2023/12/14 16:16:13 by liguyon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Zombie* zombieHorde(int N, std::string name)
 {
 	Zombie* horde =
-		new (std::nothrow) Zombie[static_cast<std::size_t>(N)]()
+		new (std::nothrow) Zombie[static_cast<std::size_t>(N)]
 	;
 	if (horde)
 	{
